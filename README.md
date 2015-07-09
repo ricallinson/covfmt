d3d3646260933dd0c68ab7a2dc5e3ae4d35c0f40
Ric Allinson <capecodehq@yahoo.co.uk>
2015-07-08 17:12:37 -0700

    Clean up

--- README.md
+++ README.md
@@ -2,17 +2,14 @@
 
 _WORK IN PROGRESS_: Utility for converting the go test coverage output into the lcov format.
 
-## Dev Setup
-
-    git clone git@github.com:ricallinson/covfmt.git
-    cd ./covfmt
-    export GOPATH=<CURRENT DIR>
-    export PATH=$GOPATH/bin:$PATH
-
 ## Usage
 
-    cat ./coverage/coverage.out | covfmt > ./coverage/lcov.info
+    go test -coverprofile=coverage.out; cat coverage.out | covfmt > ./lcov.info
 
 ## Testing
 
-    go test -coverprofile=./coverage/coverage.out
+    go test -cover
+
+## Coverage
+
+    go test -coverprofile=coverage.out; go tool cover -html=coverage.out -o=index.html
--- main.go
+++ main.go
@@ -18,6 +18,56 @@ type block struct {
 	covered    int
 }
 
+func writeLcovRecord(filePath string, blocks []*block, w *bufio.Writer) {
+
+	w.WriteString("TN:\n")
+	w.WriteString("SF:" + filePath + "\n")
+
+	// Loop over functions
+	// FN: line,name
+
+	// FNF: total functions
+	// FNH: covered functions
+
+	// Loop over functions
+	// FNDA: stats,name ?
+
+	// Loop over lines
+	total := 0
+	covered := 0
+
+	// Loop over each block and extract the lcov data needed.
+	for _, b := range blocks {
+		// For each line in a block we add an lcov entry and count the lines.
+		for i := b.startLine; i <= b.endLine; i++ {
+			total++
+			if b.covered > 0 {
+				covered++
+			}
+			w.WriteString("DA:" + strconv.Itoa(i) + "," + strconv.Itoa(b.covered) + "\n")
+		}
+	}
+
+	w.WriteString("LF:" + strconv.Itoa(total) + "\n")
+	w.WriteString("LH:" + strconv.Itoa(covered) + "\n")
+
+	// Loop over branches
+	// BRDA: ?
+
+	// BRF: total branches
+	// BRH: covered branches
+
+	w.WriteString("end_of_record\n")
+}
+
+func lcov(blocks map[string][]*block, f io.Writer) {
+	w := bufio.NewWriter(f)
+	for file, fileBlocks := range blocks {
+		writeLcovRecord(file, fileBlocks, w)
+	}
+	w.Flush()
+}
+
 func parseCoverageLine(line string) (string, *block, bool) {
 	if line == "mode: set" {
 		return "", nil, false
