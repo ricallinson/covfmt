7371a6297f06c3b622b57c5b00e12589165922f4
Ric Allinson <ric@ricallinson.com>
2019-05-31 23:16:45 -0700

    Tests are failing randomly

--- main_test.go
+++ main_test.go
@@ -2,40 +2,31 @@ package main
 
 import (
 	"bufio"
-	"os"
-	"testing"
 	"bytes"
 	"io/ioutil"
-	"strings"
-	"fmt"
+	"os"
+	"testing"
 )
 
-func TestString(t *testing.T) {
-	relPath()
-	absPath()
-}
-
-func relPath() {
+func TestRelPath(t *testing.T) {
 	var b bytes.Buffer
-    w := bufio.NewWriter(&b)
+	w := bufio.NewWriter(&b)
 	file, _ := os.Open("./fixtures/count.out")
 	lcov(parseCoverage(bufio.NewReader(file), "", ""), w)
-	t, _ := ioutil.ReadFile("./fixtures/lcov-rel.info")
-	if strings.Compare(string(t), b.String()) != 0 {
-		fmt.Print(b.String())
-		panic("Relative path test failed.")
+	test, _ := ioutil.ReadFile("./fixtures/lcov-rel.info")
+	if bytes.Compare(test, b.Bytes()) != 0 {
+		t.Errorf("Relative path test failed.")
 	}
 
 }
 
-func absPath() {
+func TestAbsPath(t *testing.T) {
 	var b bytes.Buffer
-    w := bufio.NewWriter(&b)
+	w := bufio.NewWriter(&b)
 	file, _ := os.Open("./fixtures/count.out")
 	lcov(parseCoverage(bufio.NewReader(file), "/abs/path", ""), w)
-	t, _ := ioutil.ReadFile("./fixtures/lcov-abs.info")
-	if strings.Compare(string(t), b.String()) != 0 {
-		fmt.Print(b.String())
-		panic("Absolute path test failed.")
+	test, _ := ioutil.ReadFile("./fixtures/lcov-abs.info")
+	if bytes.Compare(test, b.Bytes()) != 0 {
+		t.Errorf("Absolute path test failed.")
 	}
 }
